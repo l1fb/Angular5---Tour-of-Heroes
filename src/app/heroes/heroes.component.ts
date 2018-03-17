9c97795d45343ae1c2ebaa7c8d0e9045226995c9
David Yonwook Chung <yon_gospel@hotmail.com>
2018-03-17 14:49:48 -0700

    [ng] finish setting up message services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -24,7 +24,7 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero;
   }
   
-  getHeroes(): void{
+  getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);  
   }
