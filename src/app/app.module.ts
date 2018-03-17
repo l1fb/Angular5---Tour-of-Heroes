66d5c7e4da6bc8099e79e56f0c4001c019692323
David Yonwook Chung <yon_gospel@hotmail.com>
2018-03-16 22:52:42 -0700

    [ng] applies service and creates messages component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,23 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
+import { HeroService } from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    HeroService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
@@ -8,4 +10,7 @@ export class HeroService {
 
   constructor() { }
 
+  getHeroes(): Observable<Hero[]>{
+    return of(HEROES);
+  }
 }
